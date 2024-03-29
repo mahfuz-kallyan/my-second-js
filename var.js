5135f81d84a5a8d854768a72d7bd94935ae6ee86
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-03-29 21:10:46 +0600

    first commit

---
+++ var.js
@@ -0,0 +1,24 @@
+// three types of variable
+
+// 1. number
+var first =678;
+console.log(first);
+
+var second =789;
+console.log(second);
+
+// 2. string
+var practice ="one";
+console.log(practice);
+
+var project = "two";
+console.log(project);
+
+// 3. boolean
+var mukhosh= !true;
+console.log(mukhosh);
+
+
+console.log(typeof first);
+console.log(typeof project);
+console.clear()
